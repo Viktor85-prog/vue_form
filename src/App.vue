3524104ab311a5bfbcf5e350ecd47535d96b1fab
viktor <viktor.bulkin@stremline-c.com>
2023-02-25 14:32:51 +0300

    add validation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
-    <Form />
+	<div class="hello">
+    <div class="wrapper">
+        <div class="wrapper__inner">
+            <div class="wrapper__inner-content">
+                <main class="page">
+                    <section class="authorization authorization-main">
+                        <div class="authorization-main__container">
+                            <div class="authorization-main__wrapper">
+                                <div class="authorization-main__body">
+                                    <div class="authorization__title">Welcome back!</div>
+                                    <div class="authorization__subtitle">Enter your credentials to access your account</div>
+									<Form />
+								</div>
+                            </div>
+                        </div>
+                    </section>
+                </main>   
+            </div>
+        </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 import Form from './components/Form.vue'
+import "./style.css"
+
 
 export default {
   name: 'App',
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,48 +1,72 @@
 <template>
-  <div class="hello">
-    <div class="wrapper">
-        <div class="wrapper__inner">
-            <div class="wrapper__inner-content">
-                <main class="page">
-                    <section class="authorization authorization-main">
-                        <div class="authorization-main__container">
-                            <div class="authorization-main__wrapper">
-                                <div class="authorization-main__body">
-                                    <div class="authorization__title">Welcome back!</div>
-                                    <div class="authorization__subtitle">Enter your credentials to access your account</div>
-                                    <form action="#" name="#" class="authorization__form form-authorization">
-                                        <label for="authorization-email">
-                                            <div class="form-authorization__label">Email</div>
-                                            <input type="text" class="form-authorization__input form-authorization__input_error" id="authorization-email" name="email" placeholder="name@company.com" autocomplete="off">
-                                            <div class="form-authorization__error-text">Invalid email format</div>
-                                        </label>
-                                        <label for="authorization-password">
-                                            <div class="form-authorization__label">Password</div>
-                                            <input type="text" class="form-authorization__input" id="authorization-password" name="password" placeholder="Input your password" autocomplete="off">
-                                            <!-- <div class="form-authorization__error-text">Password must be between 8 and 20 characters</div> -->
-                                        </label>
-                                        <a href="/forgot.html" class="form-authorization__link">Forgot password?</a>
-                                        <button type="submit" class="form-authorization__button">Sign in</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </main>   
-            </div>
-        </div>
-    </div>
-  </div>
+	<form @submit.prevent ="login" action="#" name="#" class="authorization__form form-authorization">
+		<label for="authorization-email">
+			<div class="form-authorization__label">Email*</div>
+			<input 
+				type="email" 
+				v-model.trim ='$v.email.$model' 
+				:class="{'form-authorization__input_error':!$v.email.required||!$v.email.email}"
+				class="form-authorization__input " 
+				id="authorization-email" 
+				name="email" 
+				placeholder="name@company.com" 
+				autocomplete="off">
+			<div v-if ="!$v.email.required" class="form-authorization__error-text">Field is required</div>
+			<div v-if ="!$v.email.email" class="form-authorization__error-text">Invalid email format</div>
+		</label>
+		<label for="authorization-password">
+			<div class="form-authorization__label">Password*</div>
+			<input 
+				type="text" 
+				v-model.trim ='$v.password.$model' 
+				:class="{'form-authorization__input_error':!$v.password.required||!$v.password.minLength}"
+				class="form-authorization__input" 
+				id="authorization-password" 
+				name="password" 
+				placeholder="Input your password" 
+				autocomplete="off">
+			<div v-if ="!$v.password.required" class="form-authorization__error-text">Field is required</div>
+			<div v-if ="!$v.password.minLength" class="form-authorization__error-text">Password must have at least {{$v.password.$params.minLength.min}} letters.</div>
+		</label>
+		<a href="/forgot.html" class="form-authorization__link">Forgot password?</a>
+		<button type="submit" class="form-authorization__button">Sign in</button>
+	</form>
 </template>
 
 <script>
-import "./style.css"
+import { required, minLength, email } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Form',
+  data() {
+	return {
+		email: '',
+		password:''
+	}
+  },
+  validations: {
+    email: {
+      required,
+	  email
+    },
+    password: {
+		required,
+		minLength: minLength(8)
+    }
+  },
+  methods: {
+	login() {
+		this.$v.$touch()
+		if(this.$v.pending || this.$v.$error) return
+		// if(this.name && this.password) {
+		// 	sendRequest()
+		// }
+	}
   }
 }
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
